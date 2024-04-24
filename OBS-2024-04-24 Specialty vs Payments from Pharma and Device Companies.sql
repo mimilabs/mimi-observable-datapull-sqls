d4add0945394e7795c9a08ebc7cc4440c49a706b
yubinmimi <yubinmimi>
2024-04-24 23:14:21 +0000

    typo fix

--- OBS-2024-04-24 Specialty vs Payments from Pharma and Device Companies.sql
+++ OBS-2024-04-24 Specialty vs Payments from Pharma and Device Companies.sql
@@ -2,9 +2,9 @@
 SELECT nature_of_payment_or_transfer_of_value, 
     concat_ws('|', split_part(covered_recipient_specialty_1, "|", 2),
                 split_part(covered_recipient_specialty_1, "|", 3)) as specialty, 
-    AVG(total_amount_of_payment_usdollars) AS tot_amt,
+    AVG(total_amount_of_payment_usdollars) AS avg_amt,
     COUNT(*) AS cnt
 FROM (SELECT * FROM general WHERE covered_recipient_npi IS NOT NULL)
 GROUP BY nature_of_payment_or_transfer_of_value, 
     specialty
-    HAVING tot_amt > 5000 AND cnt > 30;
+    HAVING avg_amt > 5000 AND cnt > 30;
